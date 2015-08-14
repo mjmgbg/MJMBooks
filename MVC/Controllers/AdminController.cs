33ba249f98b7a14b087e06ac5c499f94ba20a255
Mikael Maaninka <mikael@maaninka.nu>
2015-08-14 21:58:04 +0200

    Added starting point for MVC

---
+++ MVC/Controllers/AdminController.cs
@@ -0,0 +1,137 @@
+﻿using Entities;
+using MVC.Models;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+	public class AdminController : Controller
+	{
+		private GetApiResponse<BookDetailDTO> apiModelAdLibris;
+		private GetApiResponse<BookModel> apiModelBook;
+		
+	
+
+		public AdminController()
+		{
+			apiModelAdLibris = new GetApiResponse<BookDetailDTO>();
+			apiModelBook = new GetApiResponse<BookModel>();
+		}
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public PartialViewResult Add()
+		{
+			return PartialView("_GetBookInfoFromISBN");
+		}
+
+		public PartialViewResult AddWithError(int error)
+		{
+			if (error == 1)
+			{
+				ViewBag.Error = "Du måste ange ISBN";
+			}
+			else
+			{
+				ViewBag.Error = "Något gick fel - försök igen.";
+			}
+
+			return PartialView("_GetBookInfoFromISBN");
+		}
+
+		public ActionResult EditBookList()
+		{
+			return null;
+
+			//return View("ListBooks", CacheHelper.GetAllBooks("0", "0"));
+		}
+
+
+		[HttpPost]
+		public ActionResult GetInfoFromAdlibris(string ISBN)
+		{
+			if (!string.IsNullOrWhiteSpace(ISBN))
+			{
+				var model = new BookViewModel();
+
+				apiModelAdLibris.BookForAdLibris = apiModelAdLibris.GetBookFromAdlibris(ISBN, "api/AdLibris/");
+				model.Book = apiModelAdLibris.BookForAdLibris;
+				return PartialView("_BookInfo", model);
+			}
+			
+			return RedirectToAction("AddWithError", new { error = 1 });
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> SaveBook(BookViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var savedInDb = await apiModelBook.IsBookInDb("api/DbBook?isbn=", model.Book.ISBN);
+				model.Book.AlreadyInDb = savedInDb;
+
+				if (model.Book.AlreadyInDb)
+				{
+					return View("_BookInfo", model);
+				}
+
+				apiModelBook.SaveBookToDb("api/DbBook/", model.Book);
+				return PartialView("_BookSaved");
+			}
+
+			return PartialView("_BookInfo", model);
+		}
+
+		public ActionResult Edit(int id)
+		{
+			if (id > 0)
+			{
+				var book = apiModelBook.GetBookFromDbById("api/APIDbBook/", id);
+				return View("EditBook", book);
+			}
+			return View("EditBook");
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Edit(int id, BookModel model)
+		{
+			try
+			{
+				//var book = await apiModelBook.UpdateBook("api/APIDbBook/", id.ToString(), model);
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		public ActionResult Delete(int id)
+		{
+			var book = apiModelBook.GetBookFromDbById("api/APIDbBook/", id);
+			return View("DeleteBook", book);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Delete(int id, BookModel model)
+		{
+			try
+			{
+				//await apiModelBook.DeleteBook("api/APIDbBook/", id.ToString(), model);
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+		
+	}
+}
---
+++ MVC/Models/GetApiResponse.cs
@@ -0,0 +1,188 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+	public class GetApiResponse<T> where T : class
+	{
+		private readonly string baseUri = "http://mjmbooksapi.azurewebsites.net/";
+
+		public BookDetailDTO BookForAdLibris { get; set; }
+
+		public BookViewModel Book { get; set; }
+
+		public List<BookViewModel> Books { get; set; }
+
+		public BookDetailDTO GetBookFromAdlibris(string isbn, string path)
+		{
+			BookForAdLibris = GetInfoFromWebApi(path, isbn) as BookDetailDTO;
+			return BookForAdLibris;
+		}
+
+		private T GetInfoFromWebApi(string path, string id)
+		{
+			var httpClient = new System.Net.Http.HttpClient
+			{
+				BaseAddress = new Uri(baseUri + path + id),
+				Timeout = TimeSpan.FromSeconds(60),
+			};
+			var test = baseUri + path + id;
+			try
+			{
+				HttpResponseMessage response = httpClient.GetAsync("").Result;
+				response.EnsureSuccessStatusCode();
+				if (response.IsSuccessStatusCode)
+				{
+					return response.Content.ReadAsAsync<T>().Result;
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+
+			return null;
+		}
+
+		private async Task<T> UpdateInfoFromWebApi<U>(string path, string id, U model)
+		{
+			var httpClient = new System.Net.Http.HttpClient
+			{
+				BaseAddress = new Uri(baseUri + path + id),
+				Timeout = TimeSpan.FromSeconds(60),
+			};
+
+			try
+			{
+				var response = await httpClient.PutAsXmlAsync("", model);
+				response.EnsureSuccessStatusCode();
+				if (response.IsSuccessStatusCode)
+				{
+					return response.Content.ReadAsAsync<T>().Result;
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+
+			return null;
+		}
+
+		private async Task<T> DeleteInfoFromWebApi<U>(string path, string id, U model)
+		{
+			var httpClient = new System.Net.Http.HttpClient
+			{
+				BaseAddress = new Uri(baseUri + path + id),
+				Timeout = TimeSpan.FromSeconds(60),
+			};
+			var test = baseUri + path + id;
+			try
+			{
+				var response = await httpClient.DeleteAsync("");
+				return response.Content.ReadAsAsync<T>().Result;
+			}
+			catch (Exception ex)
+			{
+			}
+			return null;
+		}
+
+		private Uri SaveInfoToWebApi<U>(string path, U model)
+		{
+			var httpClient = new System.Net.Http.HttpClient
+			{
+				BaseAddress = new Uri(baseUri + path),
+				Timeout = TimeSpan.FromSeconds(8),
+			};
+
+			try
+			{
+				var response = httpClient.PostAsXmlAsync("", model).Result;
+				response.EnsureSuccessStatusCode();
+				if (response.IsSuccessStatusCode)
+				{
+					// Get the URI of the created resource.
+					return response.Headers.Location;
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+
+			return null;
+		}
+
+		private List<T> GetListInfoFromWebApi(string path, string isbn)
+		{
+			var httpClient = new System.Net.Http.HttpClient
+			{
+				BaseAddress = new Uri(baseUri + path),
+				Timeout = TimeSpan.FromSeconds(60),
+			};
+
+			try
+			{
+				HttpResponseMessage response = httpClient.GetAsync(isbn).Result;
+				response.EnsureSuccessStatusCode();
+				if (response.IsSuccessStatusCode)
+				{
+					return response.Content.ReadAsAsync<IEnumerable<T>>().Result.ToList();
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+
+			return null;
+		}
+
+		public List<BookViewModel> GetAllBooksFromDb(string path)
+		{
+			return GetListInfoFromWebApi(path, "") as List<BookViewModel>;
+		}
+
+		public BookViewModel GetBookFromDbById(string path, int id)
+		{
+			Book = GetInfoFromWebApi(path, id.ToString()) as BookViewModel;
+			return Book;
+		}
+
+		public void SaveBookToDb(string path, BookDetailDTO model)
+		{
+			SaveInfoToWebApi(path, model);
+		}
+
+		public async Task<bool> IsBookInDb(string path, string id)
+		{
+			return bool.Parse(await GetResponseString(path + id));
+		}
+
+		public async Task<string> GetResponseString(string path)
+		{
+			var httpClient = new System.Net.Http.HttpClient
+			{
+				BaseAddress = new Uri(baseUri),
+			};
+			var test = baseUri + path;
+
+			var response = await httpClient.GetAsync(path);
+			var contents = await response.Content.ReadAsStringAsync();
+
+			return contents;
+		}
+
+		public async Task<BookViewModel> UpdateBook(string path, string id, BookViewModel book)
+		{
+			return await UpdateInfoFromWebApi(path, id, book) as BookViewModel;
+		}
+
+		public async Task<bool> DeleteBook(string path, string id, BookViewModel book)
+		{
+			var result = await DeleteInfoFromWebApi(path, id, book);
+			return true;
+		}
+	}
+}
